7c08f39758775a1d0ec79c0c068484016fcbe354
aryansingh-123 <aksingh2558@gmail.com>
2024-09-11 21:20:48 +0530

    Commit initial

---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,331 @@
+import { Container } from "./styles";
+import githubIcon from "../../assets/github.svg"
+// import DownloadApp from '../../assets/download.png'
+import externalLink from "../../assets/external-link.svg"
+import ScrollAnimation from "react-animate-on-scroll";
+
+
+export function Project() {
+  return (
+    <Container id="project">
+      <h2>My Projects</h2>
+      <div className="projects">
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              <div className="project-links">
+                <a href="https://github.com/aryansingh-123/profile_modal" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /></a>
+                <a href="https://aryansingh-123.github.io/profile_modal/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a> </div>
+            </header>
+            <div className="body">
+              <h3>Profile Modal - Connect with Me</h3>
+              <p> The project profile modal offers quick access to key communication platforms, including WhatsApp, LinkedIn, Instagram, GitHub, and phone calling. It streamlines connectivity for professional networking and personal interactions. </p>
+            </div>
+            <footer> <ul className="tech-list"> <li>HTML5</li> <li>CSS</li> <li>SQL</li> </ul> </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
+              <div className="project-links">
+                <a href="https://github.com/aryansingh-123/Stocks-Visualizing-and-Forecasting-of-Real-Time-Data" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /> </a>
+                <a href="https://github.com/aryansingh-123/Stocks-Visualizing-and-Forecasting-of-Real-Time-Data" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" /></a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Stocks-Visualizing-and-Forecasting-of-Real-Time-Data</h3>
+              <p>
+              Stock investments provide one of the highest returns in the market. Even though they are volatile in nature, one can visualize share prices and other statistical factors which helps keen investors carefully decide on which company they want to spend their earnings on.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Jupyter Notebook</li>
+                <li>Python</li>
+                <li>sklearn</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a href="https://github.com/aryansingh-123/GestureBridge" target="\_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" />
+                </a>
+                <a href="https://github.com/aryansingh-123/GestureBridge" target="\_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>GestureBridge</h3>
+              <p>
+              GestureBridge is a platform that enables seamless interaction through hand gestures, bridging the gap between physical actions and digital commands. It enhances user experience by offering intuitive control over devices and applications.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Jupyter Notebook</li>
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>Python</li>
+                <li>Javascript</li>
+                <li>php</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" />
+                </a>
+                <a href="" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Weather-Intelligence</h3>
+              <p>Weather intelligence refers to the use of advanced data analytics, machine learning, and predictive models to analyze and forecast weather patterns. By leveraging real-time and historical data, it provides actionable insights for industries like agriculture, transportation, energy, and retail.</p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Hack</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+                <title>Code</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a href="https://github.com/aryansingh-123/Razorpay" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" />
+                </a>
+                <a href="https://aryansingh-123.github.io/Razorpay/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Razorpay
+              </h3>
+              <p>Streamline payment management with a unified dashboard, enabling both online and in-person payment collection while enhancing conversion rates and minimizing</p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>HTML5</li>
+                <li>Tailwind CSS</li>
+             
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://aryansingh-123.github.io/minor_project/#" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>E-Commerce Website</h3>
+              <p>
+              The system allows users to authenticate and authorize, browse and search laptops by various categories, view detailed product information, add items to the cart, select payment and shipping options, track orders, customize laptops, and save favorite products to a wishlist.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>React</li>
+                <li>Shell</li>
+                <li>Html</li>
+                <li>css</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://justmedicalbooks.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>MedicalBooks - Book Selling Site</h3>
+              <p>
+                This website sells medical books online and is popular with medical students. Medical students use it to buy books and acquire knowledge. The site has all kinds of medical books in one place.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+                <li>Woocommerce</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://drrohitdamor.com" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Dr.Rohit Damor - Online Appointment Booking Site</h3>
+              <p>
+                This website allows people to easily book appointments with you online. It takes just a minute to schedule a meeting. This kind of site is helpful for booking appointments and not missing any potential clients. You should try this website to see how convenient and efficient it is.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://mitulindustries.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Mitul Industries - Manufacturing Industries Site</h3>
+              <p>
+              This manufacturing industry website shows off products and helps the business get more customers and information online. Making products is important but making money from them is the goal. This site can help the business make more profit.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+
+        {/* 
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://mayatmaj.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Mayatmaj - Film Production Site</h3>
+              <p>
+                This agency website promotes post production, virtual production, and video marketing services. It shows previous client work and reviews to get more business. If you need help with production, this website can give you information and connect you with the right people.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://www.smartcornersproperties.com/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>SmartCorners - Real Estate Properties Site</h3>
+              <p>
+              This real estate site displays land properties and helps buyers and sellers. Buyers can see lots of pictures, videos, and details about properties before visiting in person. It makes it easier for buyers to find what they want and helps sellers get more attention.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>               
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+        
+        {/* <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://madewithluv.in/" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>MadeWithLuv - HomeDecor Site</h3>
+              <p>
+                This website sells home decor products and helps people decorate their homes. It is easy to find what you need, whether it's furniture or decorations. The site is useful for people who want to make their homes look nice and need some help.
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Wordpress</li>
+                <li>Woocommerce</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation> */}
+
+      </div>
+    </Container>
+  );
+}
